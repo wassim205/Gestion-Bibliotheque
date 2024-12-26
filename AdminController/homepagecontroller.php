3b1447317cd69e83d5e8a3739750910245b1468e
Wassim El Mourabit <wassimelmorabit7@gmail.com>
2024-12-26 17:19:02 +0100

    GB-20 More modifications on the admin dashboard

--- AdminController/homepagecontroller.php
+++ AdminController/homepagecontroller.php
@@ -57,6 +57,53 @@ class Admin extends User{
         
     }
 
+    public function displayUsers(){
+        $database = new Database();
+        $db = $database->connect();
+
+        $query = "SELECT name, email, role FROM users";
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        return isset($results) ? $results : null;
+    
+    }
+    
+    public function displayBooks(){
+        $database = new Database();
+        $db = $database->connect();
+
+        $query = "SELECT books.title, books.author, categories.name, books.cover_image, books.summary, books.status FROM books join categories on books.category_id = categories.id";
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        return isset($results) ? $results : null;
+    
+    }
+
+    public function addBook($title, $author, $cover_image, $summary, $category_id) {
+        $database = new Database();
+        $db = $database->connect();
+    
+        $query = "INSERT INTO books (title, author, cover_image, summary, category_id) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $db->prepare($query);
+    
+        return $stmt->execute([$title, $author, $cover_image, $summary, $category_id]);
+    }
+    
+    public function displayCategories(){
+        $database = new Database();
+        $db = $database->connect();
+
+        $query = "SELECT * FROM categories";
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        return isset($results) ? $results : null;
+    }
 
 }
 
--- AdminPages/Books.php
+++ AdminPages/Books.php
@@ -1,3 +1,17 @@
+<?php
+require '../Class/DatabaseClass.php';
+require '../AdminController/homepagecontroller.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$admin = new Admin($db);
+$books = $admin->displayBooks();
+$categories = $admin->displayCategories();
+
+?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,10 +21,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body class="bg-gray-100 h-screen">
-    <div class="flex h-full">
+    <div class="flex h-full z-10">
         <aside class="w-64 bg-blue-600 text-white hidden md:block">
             <div class="p-4">
                 <h1 class="text-2xl font-bold">Dashboard</h1>
@@ -42,7 +57,9 @@
             <main class="p-6 flex-1 overflow-y-auto">
                 
             <div class="flex justify-end">
-                <button class="bg-blue-600 text-white py-1 px-3 rounded-lg">Add a book</button>
+                <form action="" method="POST">
+                <button class="bg-blue-600 text-white py-1 px-3 rounded-lg" name="addbook">Add a book</button>
+                </form>
             </div>
 
                 <div class="mt-6 bg-white rounded-lg shadow-md">
@@ -53,38 +70,78 @@
                             <th class="py-2 px-4">Image</th>
                                 <th class="py-2 px-4">Title</th>
                                 <th class="py-2 px-4">Author</th>
+                                <th class="py-2 px-4">Categorie</th>
                                 <th class="py-2 px-4">The number of times borrowed</th>
                                 <th class="py-2 px-4">Actions</th>
                             </tr>
                         </thead>
-                        <tbody id="bookTableBody">
+                        <tbody>
+                        <?php foreach($books as $book) : ?>
                             <tr class="border-b hover:bg-gray-100">
-                            <td class="py-2 px-4"></td>
-                                <td class="py-2 px-4 cursor-pointer hover:text-blue-700 hover:font-bold hover:underline">E-commerce Website</td>
-                                <td class="py-2 px-4">Jane Doe</td>
-                                <td class="py-2 px-4 font-bold text-red-500">15</td>
-                                <td class="py-2 px-4"><a href="#" class="text-blue-600 hover:underline">Details</a></td>
+                                <td class="py-2 px-4"><img src="<?php echo $book['cover_image']; ?>" alt="" width="50"></td>
+                                <td class="py-2 px-4 cursor-pointer hover:text-blue-700 hover:font-bold hover:underline"><?php echo $book['title']; ?></td>
+                                <td class="py-2 px-4"><?php echo $book['author']; ?></td>
+                                <td class="py-2 px-4"><?php echo $book['name']; ?></td>
+                                <td class="py-2 px-4"><?php echo $book['summary']; ?></td>
+                                <td class="py-2 px-4"><?php echo $book['status']; ?></td>
+                                <td class="py-2 px-4">
+                                    <a href="#" class="text-blue-600 hover:underline mx-3 hover:text-blue-700 hover:underline">Modifier</a>
+                                    <a href="#" class="text-red-600 hover:underline">Delete</a>
+                                </td>
                             </tr>
-                            <tr class="border-b hover:bg-gray-100">
-                            <td class="py-2 px-4"></td>
-                                <td class="py-2 px-4 cursor-pointer hover:text-blue-700 hover:font-bold hover:underline">Portfolio Website</td>
-                                <td class="py-2 px-4">John Smith</td>
-                                <td class="py-2 px-4 font-bold text-red-500">14</td>
-                                <td class="py-2 px-4"><a href="#" class="text-blue-600 hover:underline">Details</a></td>
-                            </tr>
-                            <tr class="hover:bg-gray-100">
-                            <td class="py-2 px-4"></td>
-                                <td class="py-2 px-4 cursor-pointer hover:text-blue-700 hover:font-bold hover:underline">Blog Website</td>
-                                <td class="py-2 px-4 ">Alice Johnson</td>
-                                <td class="py-2 px-4 font-bold text-red-500">5</td>
-                                <td class="py-2 px-4"><a href="#" class="text-blue-600 hover:underline">Details</a></td>
-                            </tr>
+                        <?php endforeach ?>
                         </tbody>
                     </table>
                 </div>
             </main>
         </div>
     </div>
+    
+    <?php if (isset($_POST['addbook'])) : ?>
+    <div id="formOverlay" class="fixed flex flex-col w-full h-full top-0 items-center justify-center bg-black bg-opacity-95">
+        <div class="flex justify-center items-center w-full px-4">
+            <h1 class="text-2xl font-bold mb-6 mr-32 text-center text-blue-800">Insert New Book</h1>
+            <i class="fa-solid fa-xmark text-white cursor-pointer mb-4 mr-6" onclick="closeForm()" style="font-size: 24px;"></i>
+        </div>
+        <form action="../AdminController/Gestion_book.php" method="POST" class="space-y-4 w-1/3">
+            <div>
+                <label for="title" class="block text-md font-medium text-white">Book Title</label>
+                <input type="text" id="title" name="title" class="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+            </div>
+            <div>
+                <label for="author" class="block text-md font-medium text-white">Book Author</label>
+                <input type="text" id="author" name="author" class="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+            </div>
+            <div>
+                <label for="cover_image" class="block text-md font-medium text-white">Cover Image Link</label>
+                <input type="url" id="cover_image" name="cover_image" placeholder="https://example.com/image.jpg" class="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+            </div>
+            <div>
+                <label for="summary" class="block text-md font-medium text-white">Summary</label>
+                <textarea id="summary" name="summary" rows="5" class="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"></textarea>
+            </div>
+            <div>
+                <label for="category" class="block text-md font-medium text-white">Category</label>
+                <select type="text" id="category" name="category" class="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+                    <?php foreach($categories as $category) : ?> 
+                        <option value="<?php echo $category['id']; ?>"><?php echo $category['name'] ?></option>
+                    <?php endforeach ?>
+                </select>
+            </div>
+            <div>
+                <button type="submit" class="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md shadow" name="insertBook">Insert Book</button>
+            </div>
+        </form>
+    </div>
+<?php endif ?>
+
+<script>
+    function closeForm() {
+        document.getElementById('formOverlay').style.display = 'none';
+    }
+</script>
+
+    
 </body>
 
 </html>
